b40b166fb187127cec7f0fb15298041c661ee506
uni123234 <136456996+uni123234@users.noreply.github.com>
2023-06-16 20:34:12 +0300

    Update Lifеcellbot.py

--- Lifеcellbot.py
+++ Lifеcellbot.py
@@ -9,17 +9,15 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    user_tag = f"<b>{message.from_user.full_name}</b>"
+    user_tag = f"<b>{message.from_user.username}</b>"
     await message.answer(f"Привіт, {user_tag}! Я бот від Lifеcellbot тут ви можете оплати тариф і тд ",
                          parse_mode='HTML')
 
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton('Тариф 1'))
-    keyboard.add(KeyboardButton('Тариф 2'))
-    keyboard.add(KeyboardButton('Тариф 3'))
-    keyboard.add(KeyboardButton('Тариф 4'))
-    keyboard.add(KeyboardButton('Тариф 5'))
-    await message.answer('Виберіть тариф:', reply_markup=keyboard)
+    keyboard.add(KeyboardButton('Меню'))
+    keyboard.add(KeyboardButton('Тариф'))
+    keyboard.add(KeyboardButton('Помощ'))
+    await message.answer('Виберіть пункт:', reply_markup=keyboard)
 
 
 if __name__ == '__main__':
