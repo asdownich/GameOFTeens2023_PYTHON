94898843703233c14130c7141f4a49cc65ce520e
uni123234 <136456996+uni123234@users.noreply.github.com>
2023-06-16 21:22:43 +0300

    Update Lifеcellbot.py

--- Lifеcellbot.py
+++ Lifеcellbot.py
@@ -1,22 +1,34 @@
-from aiogram import Dispatcher, Bot, types, executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import Dispatcher, Bot, types, executor
 import logging
 
-bot = Bot("6056717794:AAE8KjnkQXAQkDrXScqfUVX6Vmi4q1hP9mo")
+bot = Bot("6128664648:AAEPCBcwaF1SckoPdio4YoRfLFRRRtxK7bk")
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 
+main_menu_buttons = [
+    KeyboardButton('Меню'),
+    KeyboardButton('Тариф'),
+    KeyboardButton('Помощ')
+]
+
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    user_tag = f"<b>{message.from_user.full_name}</b>"
-    await message.answer(f"Привіт, {user_tag}! Я бот від Lifеcellbot тут ви можете оплати тариф і тд ",
+    user_tag = f"<b>{message.from_user.username}</b>"
+    await message.answer(f"Привіт, {user_tag}! Я бот від Lifеcellbot тут ви можете оплатити тариф і тд ",
                          parse_mode='HTML')
 
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton('Список тарифів.'))
-    keyboard.add(KeyboardButton('Конструктор Тарифу.'))
-    keyboard.add(KeyboardButton('Звернутися у підтримку.'))
+    keyboard.add(*main_menu_buttons)
+    await message.answer('Виберіть пункт:', reply_markup=keyboard)
+
+
+@dp.message_handler(text='Назад')
+async def go_back(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*main_menu_buttons)
     await message.answer('Виберіть пункт:', reply_markup=keyboard)
 
 
