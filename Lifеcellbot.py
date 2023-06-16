7d78c0314e0ed945ac2874492dff097ad72adae3
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-06-16 22:04:57 +0300

    Update Lifеcellbot.py

--- Lifеcellbot.py
+++ Lifеcellbot.py
@@ -1,15 +1,22 @@
+#сделать ссылку на покупку тарифов под их описаниями
+#сделать конструктор тарифа по примеру с сайта: https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/handmade/
+#косметическая доработка бота(кнопки назад, текст)
+#проверка, при необходимости доработка
+#запись екрана с работой бота
+#собрать всё в файл на гитхабе и мы полностью готовы
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import Dispatcher, Bot, types, executor
 import logging
 
-bot = Bot("6128664648:AAEPCBcwaF1SckoPdio4YoRfLFRRRtxK7bk")
+bot = Bot("6056717794:AAE8KjnkQXAQkDrXScqfUVX6Vmi4q1hP9mo")
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 
 main_menu_buttons = [
-    KeyboardButton('Тариф'),
-    KeyboardButton('Помощ')
+    KeyboardButton('Обрати Тариф'),
+    KeyboardButton('Створити свій тариф'),
+    KeyboardButton('Допомога')
 ]
 
 tariff_buttons = [
@@ -18,14 +25,13 @@ tariff_buttons = [
     KeyboardButton('Просто Лайф'),
     KeyboardButton('Platinum Лайф'),
     KeyboardButton('Шкільний Лайф'),
-    KeyboardButton('Шкільний Лайф'),
     KeyboardButton('Ґаджет'),
     KeyboardButton('Смарт Сімя'),
     KeyboardButton('Назад')
 ]
 
 tariff_descriptions = {
-    'Вільний Лайф': 'Опис Вільний Лайф',
+    'Вільний Лайф': '\n180 грн на місяць\nБезлімітний інтернет\n1600 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
     'Смарт Лайф': 'Опис Смарт Лайф',
     'Просто Лайф': 'Опис Просто Лайф',
     'Platinum Лайф': 'Опис Platinum Лайф',
@@ -46,7 +52,7 @@ async def start(message: types.Message):
     await message.answer('Виберіть пункт:', reply_markup=keyboard)
 
 
-@dp.message_handler(text='Тариф')
+@dp.message_handler(text='Обрати Тариф')
 async def show_tariffs(message: types.Message):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*tariff_buttons)
@@ -60,7 +66,7 @@ async def show_tariff_description(message: types.Message):
     await message.answer(f'{tariff}: {description}')
 
 
-@dp.message_handler(text='Помощ')
+@dp.message_handler(text='Допомога')
 async def help(message: types.Message):
     url = 'https://www.lifecell.ua/uk/pidtrimka/'
     keyboard = InlineKeyboardMarkup()
