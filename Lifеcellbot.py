36faf0c4083827279e3379a89cf5a177ae52f9aa
darkomon <ana1lphaon@gmail.com>
2023-06-16 20:32:30 +0300

    commit

--- Lifеcellbot.py
+++ Lifеcellbot.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher, Bot, types, executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import logging
 
 bot = Bot("6056717794:AAE8KjnkQXAQkDrXScqfUVX6Vmi4q1hP9mo")
@@ -8,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    user_tag = f"<b>{message.from_user.username}</b>"
+    user_tag = f"<b>{message.from_user.full_name}</b>"
     await message.answer(f"Привіт, {user_tag}! Я бот від Lifеcellbot тут ви можете оплати тариф і тд ",
                          parse_mode='HTML')
 
