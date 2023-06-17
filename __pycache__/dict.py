4dde652066b78e1edc40abf509bb6d6837d4e4de
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-06-17 17:02:19 +0300

    Create dict.py

---
+++ __pycache__/dict.py
@@ -0,0 +1,27 @@
+dict = {
+    'Free':{
+        'user':'Для себе',
+        'internet': 'Безліміт',
+        'calls':'1600 хв'
+    },
+    'Smart':{
+        'user':'Для себе',
+        'internet': '25 ГБ',
+        'calls':'800 хв'
+    },
+    'Prosto':{
+        'user':'Для себе',
+        'internet': '8 ГБ',
+        'calls':'300 хв'
+    },
+    'Platinum':{
+        'user':'Для себе',
+        'internet': 'Безліміт',
+        'calls':'3000 хв'
+    },
+    'School':{
+        'user':'Для себе',
+        'internet': '7 ГБ',
+        'calls':'Безліміт'
+    }
+}
