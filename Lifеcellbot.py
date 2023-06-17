128a3452cd96fbb60fb04d205a2b0e10c01296d9
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-06-17 11:24:39 +0300

    Update Lifеcellbot.py

--- Lifеcellbot.py
+++ Lifеcellbot.py
@@ -8,7 +8,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 from aiogram import Dispatcher, Bot, types, executor
 import logging
 
-#6056717794:AAE8KjnkQXAQkDrXScqfUVX6Vmi4q1hP9mo
 BOT_TOKEN = Bot("6056717794:AAE8KjnkQXAQkDrXScqfUVX6Vmi4q1hP9mo")
 dp = Dispatcher(BOT_TOKEN)
 
@@ -27,7 +26,7 @@ tariff_buttons = [
     KeyboardButton('Platinum Лайф'),
     KeyboardButton('Шкільний Лайф'),
     KeyboardButton('Ґаджет'),
-    KeyboardButton('Смарт Сімя'),
+    KeyboardButton('Смарт Сім\'я'),
     KeyboardButton('Назад')
 ]
 
@@ -36,6 +35,12 @@ gadjet_buttons = [
     KeyboardButton('Ґаджет Смарт'),
     KeyboardButton('Ґаджет Планшет'),
     KeyboardButton('Ґаджет Роутер')
+]
+
+family_buttons = [
+    KeyboardButton('Смарт Сім\'я S'),
+    KeyboardButton('Смарт Сім\'я M'),
+    KeyboardButton('Смарт Сім\'я L'),
 ]
 
 tariff_descriptions = {
@@ -47,12 +52,16 @@ tariff_descriptions = {
 }
 
 gadjet_descriptions = {
-    'Ґаджет Безпека': '\n90 грн на місяць\n150 МБ інтернетy/на день\n15 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
-    'Ґаджет Смарт': '\n150 грн на місяць\n500 МБ інтернетy/на день\n50 смс/на день\n50 хв на lifecell/день\nБезліміт на lifecell після використання хвилин на всі номери',
-    'Ґаджет Планшет': '\n275 грн на місяць\n50 ГБ інтернетy\n0 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
-    'Ґаджет Роутер': '\n375 грн на місяць\nБезлімітний інтернет\n0 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
+    'Ґаджет Безпека\(Для датчиків руху та GPS/GSM-сигналізації\)': '\n90 грн на місяць\n150 МБ інтернетy/на день\n15 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
+    'Ґаджет Смарт\(Для смарт-годинників\)': '\n150 грн на місяць\n500 МБ інтернетy/на день\n50 смс/на день\n50 хв на lifecell/день\nБезліміт на lifecell після використання хвилин на всі номери',
+    'Ґаджет Планшет\(Для планшетів\)': '\n275 грн на місяць\n50 ГБ інтернетy\n0 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
+    'Ґаджет Роутер\(Для модемів та маршрутизаторів \(роутерів\)\)': '\n375 грн на місяць\nБезлімітний інтернет\n0 хв на всі номери по Україні (міські, мобільні, lifecell)\nБезліміт на lifecell після використання хвилин на всі номери',
 } 
-
+family_descriptions = {
+    'Смарт Сім\'я S': '\n375 грн на місяць\n20 ГБ інтернетy\n500 хв на інші номери по Україні, безліміт на lifecell)\n500 SMS на мобільні номери по Україні',
+    'Смарт Сім\'я M': '\n425 грн на місяць\n30 ГБ інтернетy\n750 хв на інші номери по Україні, безліміт на lifecell)\n1000 SMS на мобільні номери по Україні',
+    'Смарт Сім\'я L': '\n500 грн на місяць\n50 ГБ інтернетy\n1500 хв на інші номери по Україні, безліміт на lifecell)\n1500 SMS на мобільні номери по Україні'
+}
 #bot commands
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -64,36 +73,63 @@ async def start(message: types.Message):
     keyboard.add(*main_menu_buttons)
     await message.answer('Виберіть пункт:', reply_markup=keyboard)
 
-@dp.message_handler(content_types=['text'])
-async def text(message: types.Message):
-    msg = message.text
+#tarif choosing
+@dp.message_handler(text='Обрати Тариф')
+async def show_tariffs(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*tariff_buttons)
+    await message.answer('Виберіть тариф:', reply_markup=keyboard)
 
-    if msg == 'Обрати Тариф':
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*tariff_buttons)
-        await message.answer('Виберіть тариф:', reply_markup=keyboard)
 
-    elif msg in tariff_descriptions.keys():
-        await message.answer(f'{msg}: {tariff_descriptions.get(msg)}')
+@dp.message_handler(text=[tariff for tariff in tariff_descriptions.keys()])
+async def show_tariff_description(message: types.Message):
+    tariff = message.text
+    description = tariff_descriptions.get(tariff)
+    await message.answer(f'{tariff}: {description}')
 
-    elif msg == 'Допомога':
-        url = 'https://www.lifecell.ua/uk/pidtrimka/'
-        keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton('Перейти до підтримки', url=url))
-        await message.answer('Якщо вам потрібна додаткова допомога, перейдіть за посиланням нижче:', reply_markup=keyboard)
+#gadjet_types
+@dp.message_handler(text='Ґаджет')
+async def gadjet_types(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*gadjet_buttons)
+    await message.answer('Виберіть тип тарифу:', reply_markup=keyboard)
 
-    elif msg == 'Назад':
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*main_menu_buttons)
-        await message.answer('Виберіть пункт:', reply_markup=keyboard)
-    
-    elif msg == 'Підтримка':
-        keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton('Перейти до підтримки', url='https://www.lifecell.ua/uk/pidtrimka/'))
-        await message.answer('Якщо вам потрібна додаткова допомога, перейдіть за посиланням нижче:', reply_markup=keyboard)
+@dp.message_handler(text=[tariff for tariff in gadjet_descriptions.keys()])
+async def gadjet_description(message: types.Message):
+    tariff = message.text
+    description = gadjet_descriptions.get(tariff)
+    await message.answer(f'{tariff}: {description}')
 
-    elif msg == 'Ґаджет':
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*gadjet_buttons)
-        await message.answer('Виберіть тип тарифу:', reply_markup=keyboard)
+#family_types
+@dp.message_handler(text='Смарт сім\'я')
+async def gadjet_types(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*family_buttons)
+    await message.answer('Виберіть тип тарифу:', reply_markup=keyboard)
 
-    elif msg in gadjet_descriptions.keys():
-        await message.answer(f'{msg}: {gadjet_descriptions.get(msg)}')
+@dp.message_handler(text=[tariff for tariff in family_descriptions.keys()])
+async def gadjet_description(message: types.Message):
+    tariff = message.text
+    description = family_descriptions.get(tariff)
+    await message.answer(f'{tariff}: {description}')
+#gadjet_types
+
+
+#global funcs
+@dp.message_handler(text='Назад')
+async def go_back(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*main_menu_buttons)
+    await message.answer('Виберіть пункт:', reply_markup=keyboard)
+
+
+@dp.message_handler(text='Підтримка')
+async def help(message: types.Message):
+    url = 'https://www.lifecell.ua/uk/pidtrimka/'
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton('Перейти до підтримки', url=url))
+    await message.answer('Якщо вам потрібна додаткова допомога, перейдіть за посиланням нижче:', reply_markup=keyboard)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
